0f42bd2c7fdbea0ad236c5dcaa53eb8db84c45b1
Emrane23 <omranklaai@gmail.com>
2024-12-07 02:53:35 +0100

    integration fo template

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const Encore = require('@symfony/webpack-encore');
-const webpack = require('webpack'); 
+const webpack = require('webpack');
 
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
 
-    // Ajoutez vos fichiers CSS et JS comme avant
+    // BackOffice (BO)
     .addStyleEntry('bo-styles', './assets/styles/BO/css/sb-admin-2.min.css')
     .addEntry('bo-scripts', [
         './assets/styles/BO/vendor/jquery/jquery.min.js',
@@ -13,21 +13,11 @@ Encore
         './assets/styles/BO/vendor/jquery-easing/jquery.easing.min.js',
         './assets/styles/BO/js/sb-admin-2.min.js',
     ])
-    // Scripts spécifiques aux graphiques
     .addEntry('bo-charts', [
         './assets/styles/BO/vendor/chart.js/Chart.min.js',
         './assets/styles/BO/js/demo/chart-area-demo.js',
         './assets/styles/BO/js/demo/chart-pie-demo.js',
     ])
-
-    // Ajoutez le plugin ProvidePlugin avec addPlugin()
-    .addPlugin(new webpack.ProvidePlugin({
-        $: 'jquery',          // jQuery est disponible globalement
-        jQuery: 'jquery',     // jQuery est disponible globalement
-        'window.jQuery': 'jquery', // jQuery accessible via window.jQuery
-    }))
-
-    // Autres configurations (comme la copie des fichiers)
     .copyFiles({
         from: './assets/styles/BO/img',
         to: 'images/[path][name].[ext]',
@@ -36,6 +26,35 @@ Encore
         from: './assets/styles/BO/vendor/fontawesome-free/webfonts',
         to: 'webfonts/[path][name].[ext]',
     })
+
+    .addStyleEntry('fo-styles', [
+        './assets/styles/FO/css/bootstrap.min.css',
+        './assets/styles/FO/css/boxicon.min.css',
+        './assets/styles/FO/css/custom.css',
+        './assets/styles/FO/css/templatemo.css',
+    ])
+    .addEntry('fo-scripts', [
+        './assets/styles/FO/js/jquery.min.js',
+        './assets/styles/FO/js/bootstrap.bundle.min.js',
+        './assets/styles/FO/js/custom.js',
+        './assets/styles/FO/js/templatemo.min.js',
+        './assets/styles/FO/js/isotope.pkgd.js',
+        './assets/styles/FO/js/fslightbox.js',
+    ])
+    .copyFiles({
+        from: './assets/styles/FO/img',
+        to: 'images/[path][name].[ext]',
+    })
+    .copyFiles({
+        from: './assets/styles/FO/fonts',
+        to: 'fonts/[path][name].[ext]',
+    })
+
+    .addPlugin(new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+    }))    
     .enableSingleRuntimeChunk()
     .enableSassLoader()
     .enableSourceMaps(!Encore.isProduction())
